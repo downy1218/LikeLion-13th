3fd9d22a5f9ea60d511f5d41eb5f03ca695d6694
Daeuni <ekdmsjj@gmail.com>
2025-05-23 00:46:07 +0900

    link: 라우터 연결

--- week5/src/components/homepage/Header.jsx
+++ week5/src/components/homepage/Header.jsx
@@ -1,20 +1,28 @@
 import styled from 'styled-components';
 import IDT from '../../assets/images/IDT.svg';
+import { Link } from "react-router";
+
 
 function Header(){
     return(
         <Container>
             <Img src={IDT}/>
             <Category>
-                <p>About</p>
-                <p>Project</p>
-                <p>Diary</p>
-                <p>QnA</p>
+                <Page to='/'>About</Page>
+                <Page to='/project'>Project</Page>
+                <Page to='diary'>Diary</Page>
+                <Page>QnA</Page>
             </Category>
             
         </Container>
     )
 }
+
+const Page = styled(Link)`
+    font-size:18px;
+    color:#363636;
+    text-decoration:none;
+`
 
 const Container = styled.div`
     display:flex;
